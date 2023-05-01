861336063120a4c5a2d7ed78a363cc5fbb9bfc41
sanychiquetti <sanymara77@hotmail.com>
2023-05-01 11:55:14 -0300

    criando switch

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
-import DadosPessoais from "./DadosPessais";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@material-ui/core";
 
-function FormularioCadastro({aoEnviar, validarCPF}) {
- 
-  return (
-    <DadosPessoais/>
-  )
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario aoEnviar={proximo}/>;
+      case 1:
+        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntrega aoEnviar={aoEnviar}/>;
+      default:
+        return <Typography>Erro ao selecionar formulário.</Typography>;
+    }
+  }
+
+  return <>{formularioAtual(etapaAtual)}</>;
 }
 
 export default FormularioCadastro;
