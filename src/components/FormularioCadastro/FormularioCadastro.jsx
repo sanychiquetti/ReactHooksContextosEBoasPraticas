3d5417ab658e0a6bc174c2b3d7f981f50c6cbeb4
sanychiquetti <sanymara77@hotmail.com>
2023-05-01 10:34:53 -0300

    instalando o node

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import DadosPessoais from "./DadosPessais";
+
+function FormularioCadastro({aoEnviar, validarCPF}) {
+ 
+  return (
+    <DadosPessoais/>
+  )
+}
+
+export default FormularioCadastro;
